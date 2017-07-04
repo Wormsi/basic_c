21492ef279f6222c4941a278621e65679aeb7f6a
wormsi <remidc.27@hotmail.fr>
2017-07-04 14:46:41 +0200

    m

--- exoOfUltimateEnd/mystrcmp.c
+++ exoOfUltimateEnd/mystrcmp.c
@@ -3,14 +3,12 @@ int mystrcmp(const char str1[], const char str2[])
 	int i;
 	int n;
 	i=0;
-	n=0;
-	while(str1[i]!='\0')
+	n=str1[i];
+	int p;
+	p=str2[i];
+	while(str1[i]==str2[i])
 	{
 		i++;
 	}
-	while(str2[n]!='\0')
-	{
-		n++;
-	}
-	return(i-n);
+	return(n-p);
 }
